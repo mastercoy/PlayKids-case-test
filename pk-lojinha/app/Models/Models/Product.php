c5d1348e9b35ec72a2d8e67efe48670e7801648c
Nylo F. Pinto <mastercoy@gmail.com>
2021-02-15 17:54:17 -0300

    Feature: Created some models and its migrations

---
+++ pk-lojinha/app/Models/Models/Product.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model {
+    use HasFactory;
+}
