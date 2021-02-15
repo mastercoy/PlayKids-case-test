13890df2a2cbe85b16dfe8fae1046e0269177002
Nylo F. Pinto <mastercoy@gmail.com>
2021-02-15 18:07:25 -0300

    Feature: Models, done.

--- pk-lojinha/app/Models/Category.php
+++ pk-lojinha/app/Models/Category.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Models\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function products () {
+        return $this->belongsToMany(Product::class);
+    }
 }
---
+++ pk-lojinha/app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Models;
+
+use App\Models\Category;
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model {
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function orders() {
+        return $this->belongsToMany(Order::class);
+    }
+}
