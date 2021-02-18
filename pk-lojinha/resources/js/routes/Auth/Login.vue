a73c82ae0ad80c43e9feb184c5ca87fb41a37fec
Nylo Figueira Pinto <mastercoy@gmail.com>
2021-02-18 16:42:31 -0300

    [wip] Still working on sanctum

---
+++ pk-lojinha/resources/js/routes/Auth/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="text-gray-700 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <h3>LOGIN</h3>
+        </div>
+    </section>
+
+
+</template>
+
+<script>
+export default {
+    methods: {
+        // Todo apos submeter, redirecionar
+        submit() {
+
+        }
+
+
+    },
+
+    computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pk-lojinha/resources/js/routes/Auth/Register.vue
@@ -0,0 +1,71 @@
+<template>
+    <section class="text-gray-700 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap -m-4" v-if="!products.length">
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full mb-4">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                        <img alt="ecommerce image" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260">
+                    </a>
+                    <div class="mt-4">
+                        <h3 class="text-gray-500 txt-xs tracking-widest title-font mb-1 uppercase inline-block mr-2">Ops, nenhum produto cadastrado!</h3>
+                        <h2 class="text-gray-900 title-font text-lg font-medium">Carregando</h2>
+                        <p class="mt-1">R$0,00</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-wrap -m-4" v-else>
+                <div
+                    class="lg:w-1/4 md:1/2 p-4 w-full mb-4"
+                    v-for="product in products"
+                    :key="product.id"
+                >
+                    <router-link
+                        class="block relative h-48 rounded overflow-hidden"
+                        :to="{name: 'products.show',  params: {slug: product.slug}}"
+                    >
+                        <img alt="ecommerce image" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260">
+                    </router-link>
+                    <div class="mt-4">
+                        <h3
+                            class="text-gray-500 txt-xs tracking-widest title-font mb-1 uppercase inline-block mr-2"
+                            v-for="category in product.categories"
+                            v-text="category.name"
+                        ></h3>
+                        <h2
+                            class="text-gray-900 title-font text-lg font-medium"
+                            v-text="product.name"
+                        ></h2>
+                        <p
+                            class="mt-1"
+                            v-text="formatCurrency(product.price)"
+                        ></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</template>
+
+<script>
+export default {
+    methods: {
+        formatCurrency(price) {
+            price = (price/ 100);
+            return price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+        }
+
+    },
+
+    computed: {
+        products() {
+            return this.$store.state.products;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
