b651b0c3f213c3ffd131d0240a64b33a51fefc50
Nylo Figueira Pinto <mastercoy@gmail.com>
2021-02-19 14:34:24 -0300

    Feature: login template

--- pk-lojinha/resources/js/routes/Auth/Login.vue
+++ pk-lojinha/resources/js/routes/Auth/Login.vue
@@ -1,19 +1,66 @@
 <template>
-    <section class="text-gray-700 body-font">
-        <div class="container px-5 py-24 mx-auto">
-            <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Endereço de E-mail</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="min-h-screen flex justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-md w-full space-y-8">
+            <div>
+                <img
+                    class="mx-auto h-12 w-auto"
+                    src="https://s3.amazonaws.com/playkids-blog-assets/wp-content/uploads/2020/07/10132345/logo-playkids.png"
+                    alt="Workflow"
+                >
+                <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                    Entre para acessar seu perfil!
+                </h2>
+                <p class="mt-2 text-center text-sm text-gray-600">
+                    Ou
+                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                        Crie uma nova conta!
+                    </a>
+                </p>
+            </div>
+            <!--            //fixme-->
+            <form class="mt-8 space-y-6" action="#" method="POST">
+                <input type="hidden" name="remember" value="true">
+                <div class="rounded-md shadow-sm -space-y-px">
+                    <div>
+                        <label for="email-address" class="sr-only">Endereço de Email</label>
+                        <input
+                            id="email-address" name="email" type="email" autocomplete="email" required
+                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500
+                                text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                            placeholder="Endereço de e-mail">
+                    </div>
+                    <div>
+                        <label for="password" class="sr-only">Senha</label>
+                        <input
+                            id="password" name="password" type="password" autocomplete="current-password" required
+                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500
+                                text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                            placeholder="Senha">
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Senha</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                <div>
+                    <button type="submit"
+                            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium
+                             rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+
+                         <svg
+                             class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                             xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 20 20"
+                             fill="currentColor"
+                             aria-hidden="true"
+                         >
+                        <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                              clip-rule="evenodd"/>
+                        </svg>
+                      </span>
+                        Logar-se
+                    </button>
                 </div>
-                <button type="submit" class="btn btn-primary">Logar</button>
             </form>
         </div>
-    </section>
+    </div>
 
 
 </template>
